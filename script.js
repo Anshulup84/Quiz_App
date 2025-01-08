1f977ba9774c3f3e40693c859704aa83e87afc55
Anshul <avenger720720@gmail.com>
2025-01-08 17:17:41 +0530

    working on customize alert

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const questionBox = document.querySelector('.question')
 const choicesBox = document.querySelector('.choices')
 const nextbtn = document.querySelector('.nextBtn')
 const scoreCard = document.querySelector('.scoreCard')
+const alert = document.querySelector('.alert')
 
 
 // Add QUESTION
@@ -32,6 +33,7 @@ const quiz = [
 
 let currentQuestionIndex = 0;
 let score = 0;
+let quizOver = false;
 
 // to show Questions on display
 const showQuestions =  () => {
@@ -62,17 +64,20 @@ const showQuestions =  () => {
 const checkAnswer = () => {
     const selectedChoice = document.querySelector('.choice.selected');
     if(selectedChoice.textContent === quiz[currentQuestionIndex].answer){
-        alert("Correct Answer")
+        // alert("Correct Answer")
+        displayAlert("Correct Answer")
         score++;
     }
     else{
-        alert("Wrong Answer")
+        // alert("Wrong Answer")
+        displayAlert("Wrong Answer")
     }
     currentQuestionIndex++;
     if(currentQuestionIndex < quiz.length){
-        showQuestions();``
+        showQuestions();
     } else {
         showScore();
+        quizOver = true
     }
 
 }
@@ -82,21 +87,38 @@ const showScore = () => {
     questionBox.textContent = "";
     choicesBox.textContent =  "";
     scoreCard.textContent = `You Scored ${score} out of ${quiz.length}!`
+    displayAlert("You have completed this quiz!");
     nextbtn.textContent = "Play Again";
-    nextbtn.addEventListener('click', () => {
-        currentQuestionIndex = 0;
-        showQuestions();
-        nextbtn.textContent = "Next";
-        scoreCard.textContent = ""
-    })
+   
+}
+
+// Function to Show alert
+const displayAlert = (msg) => {
+    alert.style.display = "block"
+    alert.textContent = msg;
+    setTimeout(()=> {
+      alert.style.display = "none"
+    }, 2000)
 }
 
 showQuestions();
 nextbtn.addEventListener('click', ()=> {
     const selectedChoice = document.querySelector('.choice.selected')
     if(!selectedChoice && nextbtn.textContent === "Next"){
-        alert("Select your answer")
-    } else {
+        // alert("Select your answer")
+        displayAlert("Select your answer")
+        return
+    }
+    if(quizOver){
+        nextbtn.textContent = "Next";
+        scoreCard.textContent = ""
+        currentQuestionIndex = 0;
+        showQuestions();
+        quizOver = false;
+        score = 0;
+    }
+    
+    else {
         checkAnswer();
     }
   
